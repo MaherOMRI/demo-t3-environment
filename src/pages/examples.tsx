de4e011181d82f7f5cc9fd544dd74b5819a21c0d
Maher OMRI <maher.omri99@gmail.com>
2023-06-14 20:59:20 +0100

    API Validation

--- src/pages/examples.tsx
+++ src/pages/examples.tsx
@@ -1,16 +1,56 @@
-import React from 'react'
-import { vanillaApi } from '~/utils/api'
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import React from 'react';
+import { vanillaApi } from '~/utils/api';
+import { useQuery, useMutation, QueryClient, QueryClientProvider } from 'react-query';
+interface Example {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+const Examples: React.FC<{}> = () => {
+  const queryClient = new QueryClient(); // Create an instance of QueryClient
 
-export default function Examples() {
-    // vanillaApi.example.getAll()
+  const { data, isLoading } = useQuery<Example[], unknown>('getAll', () =>
+    vanillaApi.example.getAll()
+  );
 
-    // Task 1: Display the data from the getAll query in a list, 
-    //         while it is loading display a loading indicator (i.e. a text saying "Loading...")
-    // Task 2: Add a button to each item in the list that will delete the item, by calling,
-    // the 'deleteSingle' mutation.
-    // Task 3: Implement the 'deleteSingle' mutation, and make sure the list updates when an item is deleted
+  const deleteExample = useMutation((id) =>
+    vanillaApi.example.deleteSingle(id)
+  );
+
+  const handleDelete = async (id: string) => {
+    try {
+      await deleteExample.mutateAsync(id);
+      queryClient.invalidateQueries('getAll');
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return (
-    <div>Examples</div>
-  )
-}
+    <QueryClientProvider client={queryClient}> {/* Provide the queryClient instance */}
+      <div>
+        <h1>Examples</h1>
+        <ul>
+          {data?.map((item) => (
+            <li key={item.id}>
+              {item.id} - {item.createdAt.toISOString()} - {item.updatedAt.toISOString()}{' '}
+              <button onClick={() => handleDelete(item.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </QueryClientProvider>
+  );
+};
+
+export default Examples;
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
@@ -10,9 +14,16 @@ export const exampleRouter = createTRPCRouter({
       };
     }),
   getAll: publicProcedure.query(({ ctx }) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
     return ctx.prisma.example.findMany();
   }),
   deleteSingle: publicProcedure.mutation(({ ctx, input }) => {
-    // TODO: Complete!
+    const deletedItem = ctx.prisma.example.delete({
+      where: {
+       input
+      },
+    });
+
+    return deletedItem;
   }),
 });
